04c288e9f62b6bd2682b9c0db607d7806b82370f
Aaron Herstein <aaron.herstein@aa.com>
2022-06-16 13:55:08 -0500

    update script

--- get_env_name.py
+++ get_env_name.py
@@ -5,11 +5,8 @@ def main():
     branch_name = os.environ['BRANCH_NAME']
     pr_num = os.environ['PR_NUM']
 
-    print(branch_name)
-    print(pr_num)
     x = branch_name.rfind('-') + 1
     env_name = branch_name[:x] + pr_num
-    print(env_name.upper())
 
     return env_name.upper()
 if __name__ == "__main__":
